1a0ba2f6f460a6043770759e4834328a6e64a308
Jake Scott <scottj1123@gmail.com>
2022-02-09 12:54:44 -0800

    added wordlist and random word selection

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,24 @@
 module Main where
 
+import Control.Monad (forever)
+import Data.Char (toUpper)
+import Data.Maybe (isJust)
+import System.Exit (exitSuccess)
+import System.IO
+  ( BufferMode (NoBuffering),
+    hSetBuffering,
+    stdout,
+  )
+import System.Random (randomRIO)
+import Words (WordList (..), allWords)
+
+randomWord :: WordList -> IO String
+randomWord (WordList wl) = do
+  randomIndex <- randomRIO (0, length wl - 1)
+  return $ wl !! randomIndex
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  hSetBuffering stdout NoBuffering
+  word <- randomWord allWords
+  putStrLn word
