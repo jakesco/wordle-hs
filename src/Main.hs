ffc43a870e39877bbe6e3234f2a661f940ff82eb
Jake Scott <scottj1123@gmail.com>
2022-02-09 16:00:19 -0800

    added input validation

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Control.Monad (forever, when)
-import Data.Char (toUpper)
+import Data.Char (toLower, toUpper)
 import Data.Maybe (isJust)
 import System.Exit (exitSuccess)
 import System.IO
@@ -17,11 +17,39 @@ data Puzzle = Puzzle String [String]
 instance Show Puzzle where
   show (Puzzle p guesses) =
     case guesses of
-      [] -> show $ concatMap (pad . toUpper) p
-      (g : gs) -> show $ concatMap (pad . toUpper) g
+      [] -> concatMap (pad . toUpper) p
+      (g : gs) -> concatMap (pad . toUpper) g
+
+data ValidationError = NotAWord | NotFiveChars
+  deriving (Eq, Show)
+
+data CharacterStatus = InRightPlace | InWord | NotInWord
+  deriving (Eq, Show)
 
 pad :: Char -> String
 pad c = [' ', c, ' ']
+
+isWord :: WordList -> String -> Bool
+isWord (WordList wl) s = s `elem` wl
+
+isWord' :: String -> Bool
+isWord' = isWord allWords
+
+isFiveChars :: String -> Bool
+isFiveChars = (== 5) . length
+
+isGreen :: String -> String -> [Bool]
+isGreen answer = zipWith (==) answer
+
+isYellow :: String -> String -> [Bool]
+isYellow answer = map (`elem` answer)
+
+validateGuess :: String -> Either ValidationError String
+validateGuess guess =
+  case (isFiveChars guess, isWord' guess) of
+    (False, _) -> Left NotFiveChars
+    (_, False) -> Left NotAWord
+    (_, _) -> Right guess
 
 guessesMade :: Puzzle -> Int
 guessesMade (Puzzle _ guesses) = length guesses + 1
@@ -38,7 +66,10 @@ mkPuzzle :: String -> Puzzle
 mkPuzzle s = Puzzle s []
 
 handleGuess :: Puzzle -> String -> IO Puzzle
-handleGuess (Puzzle p guesses) guess = return $ Puzzle p (guess : guesses)
+handleGuess (Puzzle p guesses) guess = do
+  let puzzle = Puzzle p (guess : guesses)
+  print puzzle
+  return puzzle
 
 gameOver :: Puzzle -> IO ()
 gameOver puzzle@(Puzzle word guesses) =
@@ -60,9 +91,10 @@ gameLoop puzzle = forever $ do
   putStrLn $ "Enter your guess [" ++ show (guessesMade puzzle) ++ "/" ++ show maxGuesses ++ "]"
   putStr "> "
   guess <- getLine
-  if length guess == 5
-    then handleGuess puzzle guess >>= gameLoop
-    else putStrLn "Your guess must be a 5 letter word."
+  case validateGuess $ map toLower guess of
+    (Left NotFiveChars) -> putStrLn "Your guess must be a 5 letter word."
+    (Left NotAWord) -> putStrLn "Your guess is not in the word list."
+    (Right guess) -> handleGuess puzzle guess >>= gameLoop
 
 main :: IO ()
 main = do
